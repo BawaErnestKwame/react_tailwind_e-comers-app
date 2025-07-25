22d9a18d8a2997b71feeab07f37db80e3d276aba
Bawa Ernest Kwame <bawaernest926@gmail.com>
2025-07-25 01:00:49 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.css';
+
+const App = () => {
+  return (
+    <div className='bg-gray-400 text-5xl font-bold text-center items-center justify-center'>App</div>
+  )
+}
+
+export default App
